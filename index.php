145262755edf35d9f238f708f05d668dcff38fbb
Iz-Dine Amadou <2088346@talnet.nl>
2020-05-07 14:16:33 +0200

    Uitwerking van de exercise

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Netland!</title>
+</head>
+<body>
 <?php
-$host = 'localhost';
-$db   = 'netland';
 $user = 'root';
 $pass = '';
 $charset = 'utf8mb4';
@@ -11,13 +16,81 @@ $options = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
-?>
+function select($quary)
+{
+    $host = 'localhost';
+    $db   =   'netland';
+    $user =      'root';
+    $pass =          '';
+    $charset  'utf8mb4';
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::ATTR_EMULATE_PREPARES   => false,];
+ try{
+   $pdo = new PDO($dsn, $user, $pass, $options);
+ }
+catch (\PDOException $e) {
+throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+  $formatResult = array();
+  $rawResult = $pdo->query($quary);
+  while ($row = $rawResult->fetch()) {
+    $rowResult = array();
+    foreach ($row as $collum => $value) {
+      $rowResult[$collum] = $value;
+    }
+$formatResult[] = $rowResult;
+  }
+        return $formatResult;
+      }
+      ?>
+<h1>Welkom op het Netland beheerders paneel</h1>
+<h3>Series</h3>
+<table>
+<thead>
+<th>Titel</th>
+<th>Rating</th>
+<th></th>
+</thead>
+<tbody>
 <?php
-try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
-   }
-   $status = $pdo->getAttribute(PDO::ATTR_SERVER_VERSION);
-   var_dump($status);
+$rows = select('SELECT * FROM series');
+foreach ($rows as $row) {
+echo <<<EOT
+<tr>
+<td>${row['title']}</td>
+<td>${row['rating']}</td>
+<td><a href="serie.php?id=${row['id']}">Meer info</a></td>
+</tr>
+EOT;
+}
 ?>
+</tbody>
+</table>
+<h3>Films</h3>
+<table>
+<thead>
+    <th>Titel</th>
+<th>Duur</th>
+<th></th>
+</thead>
+<tbody>
+<?php
+$rows = select('SELECT * FROM films'
+);
+foreach ($rows as $row) {
+echo <<<EOT
+<tr>
+<td>${row['Title']}</td>
+<td>${row['Duur']}</td>
+<td><a href="film.php?id=${row['ID']}">Meer info</a></td>
+</tr>
+EOT;
+}
+?>
+</tbody>
+</table>
+</body>
+</html>
